7c905f96d39fa9f83bc8551c33d7f21f9298dcae
avilind <xavilind7@gmail.com>
2021-12-12 18:54:38 -0600

    LecturaDeTextoAsistente

--- app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
@@ -3,9 +3,9 @@ package com.cursosant.android.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
+import android.widget.Button
 import android.widget.TextView
-
+import java.util.*
 
     class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
@@ -17,15 +17,21 @@ import android.widget.TextView
 
             tts = TextToSpeech(this, this)
 
+            findViewById<Button>(R.id.btnPlay).setOnClickListener{speak()}
+        }
+
+        private fun speak(){
+
             var message: String = findViewById<TextView>(R.id.textView).text.toString()
-            Log.i("message textview", message)
+            tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
         }
 
         override fun onInit(status: Int) {
             if (status == TextToSpeech.SUCCESS){
-                findViewById<TextView>(R.id.textView).text = "Listo!"
+
             } else {
-                findViewById<TextView>(R.id.textView).text = "No disponible :("
+                findViewById<TextView>(R.id.textView).text = "Hello Kotlin!"
+                tts!!.setLanguage(Locale.US)
             }
 
         }
