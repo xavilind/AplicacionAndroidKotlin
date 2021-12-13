54128d6ee182f4b3e5fa8642f4a7f41b47aaf5cd
avilind <xavilind7@gmail.com>
2021-12-12 18:45:15 -0600

    TextToSpeach

--- app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
@@ -1,18 +1,32 @@
-
-
 package com.cursosant.android.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var message: String = findViewById<TextView>(R.id.textView).text.toString()
-        Log.i("message textview", message)
+    class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+        var tts: TextToSpeech? = null
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            tts = TextToSpeech(this, this)
+
+            var message: String = findViewById<TextView>(R.id.textView).text.toString()
+            Log.i("message textview", message)
+        }
+
+        override fun onInit(status: Int) {
+            if (status == TextToSpeech.SUCCESS){
+                findViewById<TextView>(R.id.textView).text = "Listo!"
+            } else {
+                findViewById<TextView>(R.id.textView).text = "No disponible :("
+            }
+
+        }
     }
-}
