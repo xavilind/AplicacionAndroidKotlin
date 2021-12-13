92bf71e82102f4c85ed051fc81d275dc8741b68f
avilind <xavilind7@gmail.com>
2021-12-12 19:01:17 -0600

    MejorasVisuales

--- app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
@@ -22,16 +22,17 @@ import java.util.*
 
         private fun speak(){
 
-            var message: String = findViewById<TextView>(R.id.textView).text.toString()
+            var message: String = findViewById<TextView>(R.id.tvStatus).text.toString()
             tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
         }
 
         override fun onInit(status: Int) {
             if (status == TextToSpeech.SUCCESS){
+                findViewById<TextView>(R.id.tvStatus).text = "Hello Kotlin!"
+                tts!!.setLanguage(Locale.US)
 
             } else {
-                findViewById<TextView>(R.id.textView).text = "Hello Kotlin!"
-                tts!!.setLanguage(Locale.US)
+                findViewById<TextView>(R.id.tvStatus).text = "No disponible :("
             }
 
         }
