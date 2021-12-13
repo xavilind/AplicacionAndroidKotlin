c8e5317922e5e6cb9015c160ecadd8f9de13ad71
avilind <xavilind7@gmail.com>
2021-12-12 18:37:50 -0600

    AccederVistaKotlin

--- app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appandroidkotlin/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.appandroidkotlin
+
+
+package com.cursosant.android.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var message: String = findViewById<TextView>(R.id.textView).text.toString()
+        Log.i("message textview", message)
     }
 }
